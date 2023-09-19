b0eab6f74b5a79b62f8839445bb0e34953a0cdcc
Rhamses <rhamses.soares@gmail.com>
2023-09-19 17:32:02 -0300

    adding oembeds

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import embeds from "astro-embed/integration";
+import svelte from "@astrojs/svelte";
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://astro-blog-template.netlify.app",
-  integrations: [mdx(), embeds()],
+  integrations: [embeds(), mdx(), svelte()],
   markdown: {
     shikiConfig: {
       theme: "nord",
