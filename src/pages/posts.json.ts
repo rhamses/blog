d757bdfc6f8093e341174a42172b83ba90a93678
Rhamses <rhamses.soares@gmail.com>
2024-05-06 19:34:04 -0300

    adding cors

--- src/pages/posts.json.ts
+++ src/pages/posts.json.ts
@@ -1,6 +1,11 @@
 export async function GET({ params, request, url, site }) {
   const posts = import.meta.glob("../data/blog-posts/*.mdx");
   const newPosts = [];
+  const corsHeaders = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
+    "Access-Control-Max-Age": "86400",
+  };
   for (const post of Object.values(posts)) {
     const p = await post();
     newPosts.push({
@@ -15,6 +20,10 @@ export async function GET({ params, request, url, site }) {
     }),
     {
       headers: {
+        ...corsHeaders,
+        "Access-Control-Allow-Headers": request.headers.get(
+          "Access-Control-Request-Headers"
+        ),
         "Content-Type": "application/json",
       },
     }
